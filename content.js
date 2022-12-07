def0c938e58a2f8ee7e57a5762e51937709f22d2
sahil1sk <khannasahil303@gmail.com>
2022-12-07 10:30:51 +0530

    extension

---
+++ content.js
@@ -0,0 +1,10 @@
+
+
+console.log("<====================== Removing Ad's ================>");
+
+// let origin = window.location.origin;
+
+// const sites  = ["tutorialspoint", "geek.com", "javapoint.com"];
+// sites.includes("Mango");
+
+setInterval(() => {document.querySelectorAll('iframe').forEach((obj) => {obj.style = "visibility: hidden"})}, 3000)
